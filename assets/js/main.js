873d5cc35418fd4f0eb95c83c82c8a5d577c8b1a
Caio Caetano <dev.caiocaetano@gmail.com>
2025-08-27 08:51:07 -0300

    Refatoração de código

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,59 @@ function taskify() {
             return;
         } //Se o campo estiver vazio, o programa não faz nada
 
-        allTasks.push(userTask); //Adiciona a tarefa na lista de tarefas
+        const id = Date.now() //Pega os milissegundos decorridos desde a Época Unix e utiliza como ID.a
+        const newTask = createTask(id, userTask) //Adiciona a uma variável as informações da tarefas criada na função createTask
+        allTasks.push(newTask); //Adiciona a variável com as informações do objeto criado na função a lista de tarefas
+        const newList = createTaskElement(userTask); //Adiciona a uma variável a função que cria a <li> no html
 
+        taskList.appendChild(newList); //Adiciona o <li> completo na lista de tarefas
+        paragraph.remove(); //Remove o parágrafo "Sua lista está vazia" após adicionar uma tarefa
+        input.value = ""; //Limpa o campo de input após adicionar uma tarefa
+
+        //Quando o botão de excluir for clicado, chamará uma função
+
+        const deleteButton = newList.querySelector(".delete__button");
+        deleteButton.addEventListener("click", function deleteTask() {
+            const index = allTasks.findIndex(function(task){
+                return task.id === id;
+            }); //Encontra o índice da tarefa na lista e salva em uma variável
+
+            // Caso o índice não seja encontrado, ele o indexOf retorna -1
+            // Se o índice for diferente de -1, remove 1 item da lista a partir desse índice
+            if (index !== -1) {
+                allTasks.splice(index, 1);
+            }
+            newList.remove(); //Remove o <li> da tela.
+
+            //Adiciona um paragrafo com a mensagem "Sua lista está vazia" caso não tenha mais tarefas na lista
+            if (allTasks.length === 0) {
+                taskList.appendChild(paragraph);
+                paragraph.innerHTML = "Sua lista está vazia"
+            }
+        });
+
+        const checkbox = newList.querySelector(".checkbox");
+        //Quando o checkbox estiver marcado ou desmarcado, chamará uma função
+        checkbox.addEventListener("change", function () {
+            if (checkbox.checked) {
+                newList.classList.add("completed"); //Adiciona uma class=completed ao <li> com css estilizado para "riscar" a tarefa
+            } else {
+                newList.classList.remove("completed"); //Remove a class=completed do <li>, removendo o "risco" da tarefa
+            }
+        });
+        console.log(allTasks)
+    }
+
+    //Criação de uma função fábrica para guardar as tarefas em um objeto
+    function createTask(id, task){
+        return {
+            id,
+            task,
+        };
+    };
+
+    //Criação de uma função responsável por criar os elementos responsáveis pelas tarefas na tela.
+    function createTaskElement(textTask) {
         let newLi = document.createElement("li") //Cria um <li> no HTML para exibir a tarefa na tela
 
         let checkbox = document.createElement("input"); //Cria um novo <input> no HTML
@@ -36,46 +87,15 @@ function taskify() {
         const year = date.getFullYear(); //Adiciona a uma variável o ano que a tarefa foi adicionada.
         let textDate = document.createTextNode(`Criada em ${day}/${month}/${year}`) //Define o texto que exibirá a data
         additionDate.appendChild(textDate); //Adiciona o texto criado ao paragrafo.
-        
-        let taskTextNode = document.createTextNode(userTask); //Adiciona a uma variável o conteúdo da tarefa em texto.
+
+        let taskTextNode = document.createTextNode(textTask); //Adiciona a uma variável o conteúdo da tarefa em texto.
         newLi.appendChild(checkbox); //Adiciona o checkbox ao campo da lista
         newLi.appendChild(taskTextNode); //Adiciona a tarefa ao campo da lista
         newLi.appendChild(additionDate); //Adiciona a data de criação ao campo da lista
         newLi.appendChild(deleteButton); //Adiciona o botão de "Excluir" ao campo da lista
 
-        taskList.appendChild(newLi); //Adiciona o <li> completo na lista de tarefas
-        paragraph.remove(); //Remove o parágrafo "Sua lista está vazia" após adicionar uma tarefa
-        input.value = ""; //Limpa o campo de input após adicionar uma tarefa
-
-        const taskText = userTask; //Cria uma cópia do texto da tarefa para usar na função de excluir
-        
-        //Quando o botão de excluir for clicado, chamará uma função
-        deleteButton.addEventListener("click", function deleteTask() {
-            const index = allTasks.indexOf(taskText); //Encontra o índice da tarefa na lista e salva em uma variável
-
-            // Caso o índice não seja encontrado, ele o indexOf retorna -1
-            // Se o índice for diferente de -1, remove 1 item da lista a partir desse índice
-            if (index !== -1) {
-                allTasks.splice(index, 1);
-            }
-            newLi.remove(); //Remove o <li> da tela.
-
-            //Adiciona um paragrafo com a mensagem "Sua lista está vazia" caso não tenha mais tarefas na lista
-            if (allTasks.length === 0) {
-                taskList.appendChild(paragraph);
-                paragraph.innerHTML = "Sua lista está vazia"
-            }
-        });
-
-        //Quando o checkbox estiver marcado ou desmarcado, chamará uma função
-        checkbox.addEventListener("change", function () {
-            if (checkbox.checked) {
-                newLi.classList.add("completed"); //Adiciona uma class=completed ao <li> com css estilizado para "riscar" a tarefa
-            } else {
-                newLi.classList.remove("completed"); //Remove a class=completed do <li>, removendo o "risco" da tarefa
-            }
-        });
-    }
+        return newLi;
+    };
 
     //Quando o botão de Adicionar for clicado, pega os dados do input e adiciona a função receiveTask.
     task.addEventListener("submit", receiveTask);
